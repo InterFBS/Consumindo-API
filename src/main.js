9aab4f97419d293e6d3b4e56acade0e6048076f8
FelipeBarroso <felipebarroso299@gmail.com>
2022-04-22 21:32:41 -0300

    Funcionando a consulta

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-const cep = document.querySelector("#cep")
+const cep = document.querySelector("#cep");
 
-cep.addEventListener("blur", ()=>{
-    console.log(cep.value)
-})
+cep.addEventListener("blur", (e) => {
+  let search = cep.value.replace("-", "");
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+
+  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then((response) => {
+      response.json()
+        .then(data => console.log(data))
+    })
+    .catch(e => console.log("Deu erro: "+ e, message))
+});
